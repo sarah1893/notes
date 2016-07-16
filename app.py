19a8ef087c91838bdcda5b4178fd3d80f2e9d783
chang-ning <spiderpower02@gmail.com>
2016-07-16 19:34:56 +0800

    Enable https redirection

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT = os.path.join(DIR, 'docs', '_build', 'html')
 
 app = Flask(__name__)
-#if 'DYNO' in os.environ:
-#        sslify = SSLify(app)
+if 'DYNO' in os.environ:
+        sslify = SSLify(app)
 
 @app.route('/<path:path>')
 def static_proxy(path):
@@ -22,10 +22,6 @@ def static_proxy(path):
 def index_redirection():
     """Redirecting index file"""
     return send_from_directory(ROOT, 'index.html')
-
-@app.route('/.well-known/acme-challenge/39LkboWlzrvSn4bGSC-vx5Ea4FqKipU5QYcLKAn1hSM')
-def letsencrypt():
-    return "39LkboWlzrvSn4bGSC-vx5Ea4FqKipU5QYcLKAn1hSM.1MQ2tN97oDMCS6VnIiDMXAjUQwpiEoyNT2mUiGLhR7o"
 
 if __name__ == "__main__":
     app.run(debug=True)
