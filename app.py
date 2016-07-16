706ea0d5fadce4cf283ad29c1daafdf1859cb1d1
chang-ning <spiderpower02@gmail.com>
2016-07-16 18:35:56 +0800

    Disable ssl

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 ROOT = os.path.join(DIR, 'docs', '_build', 'html')
 
 app = Flask(__name__)
-if 'DYNO' in os.environ:
-        sslify = SSLify(app)
+#if 'DYNO' in os.environ:
+#        sslify = SSLify(app)
 
 @app.route('/<path:path>')
 def static_proxy(path):
